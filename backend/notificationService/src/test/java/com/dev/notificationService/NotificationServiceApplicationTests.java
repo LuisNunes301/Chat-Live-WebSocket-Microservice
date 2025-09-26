4b576c645b6fa11a2aa14aac9608bb5840409808
LuisNunes301 <vnunes30@gmail.com>
2025-09-26 03:09:22 -0300

    Add 3 services ( chat/user/notification)

---
+++ backend/notificationService/src/test/java/com/dev/notificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.notificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
